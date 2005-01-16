e25382a17cc1ddf2f2df110a0d849c386b7aa3e8
James Taylor <james@jamestaylor.org>
2005-01-16 22:06:33 +0000

    First cut at MPI version of rp_adapt
    --HG--
    extra : convert_revision : svn%3A18f86cc5-19d7-0310-b00f-baa79078a90e/trunk%4020

---
+++ python-rp-scores/rp_adapt_mpi.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python2.3
+
+"""
+Collapse from a starting alphabet to a series of nested alphabets using
+cross validation as 'merit'.
+
+usage: %prog pos_data neg_data out_dir [options]
+   -f, --format=NAME:  Format of input data. 'ints' by default, or 'maf'
+   -m, --mapping=FILE: A mapping (alphabet reduction) to apply to each sequence
+"""
+
+import align.maf
+import cookbook.doc_optparse
+import os.path
+import sys
+import traceback
+
+from cookbook.progress_bar import *
+from rp import cv, io, standard_model
+
+import rp.mapping
+
+# Startup pypar and get some info about what node we are
+import pypar 
+nodes = pypar.size() 
+node_id = pypar.rank() 
+print >>sys.stderr, "I am node %d of %d" % ( nodes, node_id )
+
+STOP_SIZE = 22
+
+fold = 10
+passes = 10
+
+# FIXME: these should not be hardcoded, switch to 'averaging' strategy
+order = 1
+model = standard_model
+
+def run( pos_file, neg_file, out_dir, format, mapping ):
+
+    # Open merit output
+    merit_out = open( os.path.join( out_dir, 'merits.txt' ), 'w' )
+
+    # Read integer sequences
+    pos_strings = list( io.get_reader( pos_file, format, None ) )
+    neg_strings = list( io.get_reader( neg_file, format, None ) )
+
+    symbol_count = mapping.get_out_size()
+
+    # Collapse
+    while symbol_count > STOP_SIZE:
+
+        if node_id == 0:
+            print >> sys.stderr, "Collapsing from:", symbol_count
+
+        pairs = all_pairs( symbol_count )
+
+        # Decide which subrange of all pairs this node will handle
+        interval_size = len( pairs ) / nodes
+        lo = node_id * interval_size
+        hi = lo + interval_size
+        
+        print >> sys.stderr, ’Node %d has interval [%d, %d)’ % ( node_id, lo, hi ) 
+
+        # Find best collapsed mapping in interval
+        best_i, best_j, best_merit = None, None, 0
+        for i, j in pairs[lo:hi]:
+            merit = calc_merit( pos_strings, neg_strings, mapping.collapse( i, j )  ) 
+            if merit > best_merit:
+                
+            
+        # Aggregate results
+        if node_id != 0:
+            # Send best i, j, merit to the master
+            pypar.send( ( best_i, best_j, merit ), 0 )
+        else:
+            # I am the master, get results from all other nodes and determine
+            # which had the best merit
+            for other_node_id in range( 1, nodes ):
+                i, j, merit = pypar.receive( other_node_id )
+                if merit > best_merit:
+                    best_i, best_j, best_merit = i, j, merit
+  
+        # Collapse the two symbols that resulted in the best merit   
+        mapping = mapping.collapse( best_i, best_j )
+        symbol_count -= 1
+        
+        # Ensure only the master writes files
+        if node_id == 0:
+        
+            # Append merit to merit output        
+            print >>merit_out, symbol_count, best_merit
+        
+            print >>sys.stderr, "\nBest Merit %d." % best_merit,
+        
+            # Write best mapping to a file
+            mapping_out = open( os.path.join( out_dir, "%03d.mapping" % symbol_count ), 'w' )
+            for symbol in mapping.get_table(): print >>mapping_out, symbol
+            mapping_out.close()
+        
+def all_pairs( n ):
+    rval = []
+    for i in range( 0, n ):
+        for j in range( i + 1, n ):
+            rval.append( ( i, j ) )
+    return rval        
+
+def calc_merit( pos_strings, neg_strings, mapping ):
+    # Apply mapping to strings
+    pos_strings = [ mapping.translate( s ) for s in pos_strings ]
+    neg_strings = [ mapping.translate( s ) for s in neg_strings ]
+    # Cross validate using those strings
+    model_factory = lambda d0, d1: model.train( order, mapping.get_out_size(), d0, d1 )
+    cv_engine = cv.CV( model_factory, pos_strings, neg_strings, fold=fold, passes=passes )
+    cv_engine.run()
+    # Merit is TP + TN
+    return cv_engine.cls1.pos + cv_engine.cls2.neg
+
+def main():
+
+    # Parse command line
+
+    options, args = cookbook.doc_optparse.parse( __doc__ )
+
+    try:
+        pos_fname, neg_fname, out_dir = args
+        mapping = rp.mapping.alignment_mapping_from_file( file( options.mapping ) )
+    except:
+        cookbook.doc_optparse.exit()
+
+    run( open( pos_fname ), open( neg_fname ), out_dir, options.format, mapping )
+
+
+if __name__ == "__main__": main()
