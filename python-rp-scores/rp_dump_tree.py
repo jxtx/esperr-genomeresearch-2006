1e6d07638c5fa647ffd4df7ae62a938ddaa8bce8
James Taylor <james@jamestaylor.org>
2005-03-01 21:31:31 +0000

    Utility to dump a tree in into text
    --HG--
    extra : convert_revision : svn%3A18f86cc5-19d7-0310-b00f-baa79078a90e/trunk%4044

---
+++ python-rp-scores/rp_dump_tree.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import sys
+from elementtree.ElementTree import ElementTree, Element, parse
+
+def draw( element, level ):
+    if not element: return
+    symbol = element.get('symbol')
+    vals = map( float, element.find( "vals" ).text.split( ',' ) )
+    # Draw ascii treeness
+    if level > 1:
+        sys.stdout.write( " |  " * ( level - 1 ) )
+    if level > 0:
+        sys.stdout.write( " +--" )
+    # Symbol and vals for level
+    print "[" + str( symbol ) + "] (",
+    for val in vals: print "%0.2f" % val,
+    print "|", sum(vals),
+    print ")"
+    for child in element.find( "children" ):
+        draw( child, level+1 )
+    
+if __name__ == "__main__":
+    tree = parse( sys.argv[1] )
+    root = tree.getroot()
+    draw( root.find( "node" ), 0 )
